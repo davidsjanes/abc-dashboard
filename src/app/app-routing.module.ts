9eb6aa761e819c796e7cae8a6457ba69bb16883b
djanes <djanes376@gmail.com>
2024-11-01 13:37:04 -0500

    clean up

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,22 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { DashboardComponent } from './components/pages/dashboard/dashboard.component';
 import { OrdersComponent } from './components/pages/orders/orders.component';
 import { DeliveriesComponent } from './components/pages/deliveries/deliveries.component';
 import { TemplatesComponent } from './components/pages/templates/templates.component';
 import { QuotesComponent } from './components/pages/quotes/quotes.component';
 import { MeasurementsComponent } from './components/pages/measurements/measurements.component';
-import { DashboardPocComponent } from './components/pages/dashboard-poc/dashboard-poc.component';
 
 const routes: Routes = [
-  {path: '', component: DashboardPocComponent},
-  {path: 'home', component: DashboardPocComponent},
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: 'home', component: OrdersComponent},
   {path: 'orders', component: OrdersComponent},
   {path: 'deliveries', component: DeliveriesComponent},
   {path: 'templates', component: TemplatesComponent},
   {path: 'quotes', component: QuotesComponent},
-  {path: 'measurements', component: MeasurementsComponent},
-  {path: 'account', component: DashboardPocComponent}
+  {path: 'measurements', component: MeasurementsComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,14 +13,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { SidebarComponent } from './components/navigation/sidebar/sidebar.component';
-import { DashboardComponent } from './components/pages/dashboard/dashboard.component';
 import { TopNavComponent } from './components/navigation/top-nav/top-nav.component';
 import { FooterComponent } from './components/navigation/footer/footer.component';
 import { SiteNavComponent } from './components/navigation/site-nav/site-nav.component';
-import { OrdersWidgetComponent } from './components/pages/dashboard/widgets/orders-widget/orders-widget.component';
-import { DeliveriesWidgetComponent } from './components/pages/dashboard/widgets/deliveries-widget/deliveries-widget.component';
-import { TemplatesWidgetComponent } from './components/pages/dashboard/widgets/templates-widget/templates-widget.component';
-import { MeasurementsWidgetComponent } from './components/pages/dashboard/widgets/measurements-widget/measurements-widget.component';
 import { OrdersComponent } from './components/pages/orders/orders.component';
 import { AccountComponent } from './components/pages/account/account.component';
 import { MeasurementsComponent } from './components/pages/measurements/measurements.component';
@@ -29,18 +24,12 @@ import { MeasurementsNewPageComponent } from './components/pages/measurements/me
 import { QuotesComponent } from './components/pages/quotes/quotes.component';
 import { TemplatesComponent } from './components/pages/templates/templates.component';
 import { DeliveriesComponent } from './components/pages/deliveries/deliveries.component';
-import { OrdersWidgetRowComponent } from './components/pages/dashboard/widgets/orders-widget/orders-widget-row/orders-widget-row.component';
-import { TemplatesWidgetRowComponent } from './components/pages/dashboard/widgets/templates-widget/templates-widget-row/templates-widget-row.component';
-import { MeasurementsWidgetRowComponent } from './components/pages/dashboard/widgets/measurements-widget/measurements-widget-row/measurements-widget-row.component';
-import { DeliveriesWidgetGroupComponent } from './components/pages/dashboard/widgets/deliveries-widget/deliveries-widget-group/deliveries-widget-group.component';
-import { DeliveriesWidgetItemComponent } from './components/pages/dashboard/widgets/deliveries-widget/deliveries-widget-group/deliveries-widget-item/deliveries-widget-item.component';
 import { ModalComponent } from './components/shared/modal/modal.component';
 import { OverlayComponent } from './components/shared/overlay/overlay.component';
 import { TestContentComponent } from './components/shared/test-content/test-content.component';
 import { OrdersRowComponent } from './components/pages/orders/orders-row/orders-row.component';
 import { MeasurementsRowComponent } from './components/pages/measurements/measurements-row/measurements-row.component';
 import { OrdersFiltersComponent } from './components/pages/orders/orders-filters/orders-filters.component';
-import { DashboardPocComponent } from './components/pages/dashboard-poc/dashboard-poc.component';
 import { SlideoutComponent } from './components/shared/slideout/slideout.component';
 import { ProductNavComponent } from './components/navigation/product-nav/product-nav.component';
 import { AccountNavComponent } from './components/navigation/account-nav/account-nav.component';
@@ -49,14 +38,9 @@ import { AccountNavComponent } from './components/navigation/account-nav/account
   declarations: [
     AppComponent,
     SidebarComponent,
-    DashboardComponent,
     TopNavComponent,
     FooterComponent,
     SiteNavComponent,
-    OrdersWidgetComponent,
-    DeliveriesWidgetComponent,
-    TemplatesWidgetComponent,
-    MeasurementsWidgetComponent,
     OrdersComponent,
     DeliveriesComponent,
     TemplatesComponent,
@@ -65,11 +49,6 @@ import { AccountNavComponent } from './components/navigation/account-nav/account
     MeasurementsNewPageComponent,
     AccountComponent,
     QuotesComponent,
-    OrdersWidgetRowComponent,
-    TemplatesWidgetRowComponent,
-    MeasurementsWidgetRowComponent,
-    DeliveriesWidgetGroupComponent,
-    DeliveriesWidgetItemComponent,
     ModalComponent,
     SlideoutComponent,
     OverlayComponent,
@@ -77,7 +56,6 @@ import { AccountNavComponent } from './components/navigation/account-nav/account
     OrdersRowComponent,
     MeasurementsRowComponent,
     OrdersFiltersComponent,
-    DashboardPocComponent,
     ProductNavComponent,
     AccountNavComponent
   ],
